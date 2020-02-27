5246ad197739f5665a50d7fe6cc971baf136857d
RazzSunuwar <razzkoinch5@gmail.com>
2020-02-27 16:45:32 +0545

    Main Project

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css'
 
 // setup components
+const Person = ({img, name, job, children}) => {
+    const url = `https://randomuser.me/api/portraits/thumb/men/${img}.jpg`;
+    return(
+        <article className="person">
+            <img src ={url} alt="person" />
+            <h4>{name}</h4>
+            <h4>{job}</h4>
+            {children}
+        </article>
+    )
+}
+
 const PersonList = () => {
-    return <section>My content will be here </section>
+    return <section className="person-list">
+        <Person img="34" name="Brad" job="Developer" />
+        <Person img="40" name="Mosh" job="Data Engineer">
+            <p>Hi its me! Mosh I am data Engineer. I am working for Google from past two years.</p>
+        </Person>
+        <Person img="32" name="James" job="QA" />
+    </section> 
 }
 
 

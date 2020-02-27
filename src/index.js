7a524d70108a1fb6dec06e5787b5801cf707cf40
RazzSunuwar <razzkoinch5@gmail.com>
2020-02-27 14:00:33 +0545

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// setup components
+const PersonList = () => {
+    return <section>My content will be here </section>
+}
+
+
+ReactDOM.render(<PersonList />, document.getElementById('root'));

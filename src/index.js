bbd9aa1b9ddc1e54e95a091a769e966f3faebe27
RazzSunuwar <razzkoinch5@gmail.com>
2020-02-28 08:45:42 +0545

    Person List

--- src/index.js
+++ src/index.js
@@ -17,11 +17,15 @@ const Person = ({img, name, job, children}) => {
 
 const PersonList = () => {
     return <section className="person-list">
-        <Person img="34" name="Brad" job="Developer" />
-        <Person img="40" name="Mosh" job="Data Engineer">
-            <p>Hi its me! Mosh I am data Engineer. I am working for Google from past two years.</p>
+        <Person img="34" name="Brad" job="Developer"><p>Hi its me! Brad I am data Full Stack Developer.
+            I am working as a freelancer from past two years.</p>
         </Person>
-        <Person img="32" name="James" job="QA" />
+        <Person img="32" name="James" job="Java Developer"><p>Hi its me! James I am Java Developer.
+            I am working for Google from past two years.</p>
+        </Person>
+        <Person img="35" name="Mosh" job="Data Engineer">
+        <p>Hi its me! Mosh I am data Engineer. I am working for facebook from past two years.</p>
+        </Person>
     </section> 
 }
 
